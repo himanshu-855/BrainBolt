d08da199889614f7c6b7c2d7ecfd6c87cf5e3400
himanshu-855 <himanshuchaudhari855@gmail.com>
2025-12-04 23:33:19 +0530

    03 tRPC setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import { Button } from "@/components/ui/button";
+import { dehydrate } from "@tanstack/react-query";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-const page = () => {
+const page = async () => {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(
+    trpc.createAI({ text: "Himanshu" })
+  );
   return (
-    <div>
-     <Button variant="destructive">
-      Click Here
-      </Button>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
 
