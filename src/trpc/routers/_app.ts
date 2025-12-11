4e909188afd717ba9fba3a5fe9e7496a940e2875
himanshu-855 <himanshuchaudhari855@gmail.com>
2025-12-11 12:39:31 +0530

    AI model Setup Done

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,37 @@
 import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "../init";
+import { inngest } from "@/inngest/client";
+import { Value } from "@radix-ui/react-select";
+
 export const appRouter = createTRPCRouter({
   createAI: baseProcedure
     .input(
       z.object({
         text: z.string(),
+        value: z.string(),
       })
     )
     .query((opts) => {
       return {
         greeting: `hello ${opts.input.text}`,
+      };
+    }),
+  invoke: baseProcedure
+    .input(
+      z.object({
+        value: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      await inngest.send({
+        name: "test/hello.world",
+        data: {
+          value: input.value,
+        },
+      });
+      return {
+        success: true,
+        message: "Background job triggered",
       };
     }),
 });
